540174f66531940f4aee27332149e6c774ef27dc
Andreas Gillström <andreasgillstrom@gmail.com>
2015-08-09 00:54:47 +0200

    Add XO

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 'use strict';
 var assert = require('assert');
+var mocha = require('mocha');
 var numArr = require('./');
 
-it('should just work', function () {
+mocha.it('should just work', function () {
 	assert.deepEqual(numArr(7), [7]);
 	assert.deepEqual(numArr(52324), [5, 2, 3, 2, 4]);
 });
