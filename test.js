070c37d409bda80c43f4578b67996538dc789d5a
Andreas Gillström <andreasgillstrom@gmail.com>
2016-01-11 21:12:23 +0100

    Use `AVA` for tests

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 'use strict';
-var assert = require('assert');
-var mocha = require('mocha');
-var numArr = require('./');
+var test = require('ava');
+var m = require('./');
 
-mocha.it('should just work', function () {
-	assert.deepEqual(numArr(7), [7]);
-	assert.deepEqual(numArr(52324), [5, 2, 3, 2, 4]);
+test(function (t) {
+	t.same(m(7), [7]);
+	t.same(m(52324), [5, 2, 3, 2, 4]);
 });
